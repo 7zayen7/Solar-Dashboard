d43af94ed11ee068c5d4dfcf097d603933d5e752
7zayen7 <hzayenmohammad@gmail.com>
2024-06-30 14:48:34 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -349,8 +349,8 @@ def create_budget_allocation_chart(df):
         paper_bgcolor="white"
     )
     fig.update_layout(
-        width=1800,  # Adjust the width as needed
-        height=1200  # Adjust the height as needed
+        width=1500,  # Adjust the width as needed
+        height=1000  # Adjust the height as needed
     )
     return fig.to_image(format="png")
 
